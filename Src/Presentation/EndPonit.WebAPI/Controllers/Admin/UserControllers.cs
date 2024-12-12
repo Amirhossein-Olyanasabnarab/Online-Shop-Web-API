fcfd52ecbed101917e0bf6ffd77848cc1b9eb17c
amirholya01 <amirholya01@gmail.com>
2024-12-12 18:41:12 +0100

    Refactoring code about Users

---
+++ Src/Presentation/EndPonit.WebAPI/Controllers/Admin/UserControllers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.Services.Users.Queries;
+
+namespace EndPonit.WebAPI.Controllers.Admin;
+
+[ApiController]
+[Route("/api/admin/[controller]")]
+public class UserControllers : ControllerBase
+{
+    private readonly IGetUserService _getUserService;
+
+    public UserControllers(IGetUserService getUserService)
+    {
+        _getUserService = getUserService;
+    }
+
+    [HttpGet("Index")]
+    public IActionResult GetUsers([FromQuery] string searchKey, [FromQuery] int page = 1)
+    {
+        var result = _getUserService.Execute(new RequestGetUserDto
+        {
+            Page = page,
+            SearchKey = searchKey
+        });
+
+        return Ok(result);
+    }
+}
--- Src/Presentation/EndPonit.WebAPI/Program.cs
+++ Src/Presentation/EndPonit.WebAPI/Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using OnlineShop.Application.Interfaces.Contexts;
+using OnlineShop.Application.Services.Users.Queries;
 using OnlineShop.Persistence.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddDbContext<DataBaseContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddScoped<IGetUserService, GetUserService>();
+builder.Services.AddScoped<IDataBaseContext, DataBaseContext>();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "Online Shop", Version = "v1"});
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,7 +28,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 
 
 app.Run();
